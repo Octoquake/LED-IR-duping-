afccd5712ba5c08e36551d8e8e9573b12ccfac8a
Octoquake <ryanmatthewm@gmail.com>
2016-01-12 21:33:10 -0600

    RPi python binary output

---
+++ python scripts/02-brightnessdown.py
@@ -0,0 +1,219 @@
+import RPi.GPIO as GPIO
+import fileinput
+from time import sleep
+import time
+import datetime
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#GPIO Settings
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.OUT)
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#Variables
+##START 
+##	GPIO.output(26, 0)
+##	sleep(0.009)
+##	GPIO.output(26, 1)
+##	sleep(0.0045)
+
+##0BIT
+##	GPIO.output(26, 0)
+##	sleep(0.00056)
+##	GPIO.output(26, 1)
+##	sleep(0.00056)
+
+##1BIT
+##	GPIO.output(26, 0)
+##	sleep(0.00056)
+##	GPIO.output(26, 1)
+##	sleep(0.00169)
+
+##STOP
+##	GPIO.output(26, 0)
+##	sleep(0.009)
+##	GPIO.output(26, 1)
+##	sleep(0.0045)
+
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#Main Code
+while True:
+	##START BIT
+	GPIO.output(26, 0)
+	sleep(0.009)
+	GPIO.output(26, 1)
+	sleep(0.0045)
+	
+	##ADDRESS 00000000
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	##INVERT ADDRESS 11111111
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	##COMMAND 10111010
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)	
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	##INVERT COMMAND 01000101
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)	
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00056)
+
+	GPIO.output(26, 0)
+	sleep(0.00056)
+	GPIO.output(26, 1)
+	sleep(0.00169)
+	
+
+	##STOP BIT
+	GPIO.output(26, 0)
+	sleep(0.009)
+	GPIO.output(26, 1)
+	sleep(0.0045)	
+		
+	print('brightness -')
+	break
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
